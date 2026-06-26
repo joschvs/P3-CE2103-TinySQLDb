37581031a9c15207b5b1c8331c94cfc5ee150c0c
Jose Chaves Chacón <jmch0406@gmail.com>
2026-06-26 15:31:21 -0600

    feat: add main.cpp and enable Windows 10 flag for httplib

--- server/main.cpp
+++ server/main.cpp
@@ -1,5 +1,18 @@
+#include "TinySQLDb.h"
+#include "storage/StoredDataManager.h"
+#include "QueryProcessor.h"
+#include "WebAPI.h"
 #include <iostream>
 
-int main()
-{
+int main() {
+    std::cout << "Iniciando TinySQLDb...\n";
+
+    StoredDataManager sdm;       // carga el System Catalog y reconstruye los índices
+    QueryProcessor qp(sdm);
+    WebAPI api(qp, 8080);
+
+    std::cout << "Servidor escuchando en el puerto 8080...\n";
+    api.run();
+
+    return 0;
 }
